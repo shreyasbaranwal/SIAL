761f1e172ae564d164133ef9e5b847082576009b
David Tyrpak <tyrpak@usc.edu>
2020-03-17 19:19:54 -0700

    Update LogFile_Tests.java
    added test cases for whichFile() method and for harvestMetaData() (this one isnt correctly working)

--- src/test/java/SIAL/LogFile_Tests.java
+++ src/test/java/SIAL/LogFile_Tests.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.HashMap;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,6 +32,20 @@ public class LogFile_Tests {
 		 
 		 assertTrue(exLogFile.getExtension() == extension);
 	}
+	
+	@Test
+	public void correct_whichFile() throws IOException {
+		File log = output_folder.newFile("myLogFile.txt");
+		 
+		 String extension = "tiff";
+		  
+		 LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		 
+		 assertTrue(exLogFile.whichFile() == log) ;
+		
+	}
+	
+	
 	
 	
 	//Ensure that metadata and file names are correctly written to log file
@@ -73,8 +87,48 @@ public class LogFile_Tests {
 	  
 	  assertTrue(lastFileBoolean == lastMetaDataBoolean); }
 	 
-	
-}
+	 
+	//Ensure that the correct number of metadata lines are retrieved. 
+	  //In this test the metadata is correctly written without any whitespace between the colon delimiters, and the metadata lines are added first
+	@Test
+	public void test_harvestMetaDataSize_noWhitespace_metaFirst() throws IOException {
+		File fileOne = input_folder.newFile("1.czi");
+		  
+		  File fileTwo = input_folder.newFile("2.czi");
+		  
+		  File fileThree = input_folder.newFile("3.czi");
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+		  //write metadata 
+		  exLogFile.writeMetadata("input_directory:" + exLogFile.getInputDirectory().toString());
+		  
+		  exLogFile.writeMetadata("output_directory:" + exLogFile.whichFile().getParent());
+		  
+		  exLogFile.writeMetadata("extension:" + exLogFile.getExtension());
+		  		  
+		  //write file names (pretending we analyzed these files)
+		  exLogFile.appendLine(fileOne.getName());
+		  
+		  exLogFile.appendLine(fileTwo.getName());
+		  
+		  exLogFile.appendLine(fileThree.getName());
+		  
+		  HashMap<String, String> metaDataMap = exLogFile.harvestMetaData();
+		  
+		  System.out.println("Size is: " + metaDataMap.keySet().size());
+		  System.out.println("map:" + metaDataMap);
+		  
+		  //assertTrue(metaDataMap.size() == 3);
+		  
+		  
+		
+		 }
+	 }
 	
 
 
