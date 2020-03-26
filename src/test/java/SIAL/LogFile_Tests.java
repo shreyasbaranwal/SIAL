5e36a3fa097262fe892f616e173f373fb6a74dd0
David Tyrpak <tyrpak@usc.edu>
2020-03-25 19:03:23 -0700

    Update LogFile_Tests.java
    Added new test cases for countMetaDataLines() and countAnalyzedFiles()

--- src/test/java/SIAL/LogFile_Tests.java
+++ src/test/java/SIAL/LogFile_Tests.java
@@ -206,11 +206,273 @@ public class LogFile_Tests {
 		
 		 }
 	
+	//Ensure that the correct number of metadata lines are retrieved
+	@Test
+	public void test_countMetaDataLines() throws IOException {
+		
+		File fileOne = input_folder.newFile("1.czi");
+		  
+		  File fileTwo = input_folder.newFile("2.czi");
+		  
+		  File fileThree = input_folder.newFile("3.czi");
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+		//write metadata 
+		  exLogFile.writeMetaData("input_directory", exLogFile.getInputDirectory().toString());
+		  
+		  exLogFile.writeMetaData("output_directory", exLogFile.whichFile().getParent());
+		  
+		  exLogFile.writeMetaData("extension", exLogFile.getExtension());
+		  
+		//write file names (pretending we analyzed these files)
+		  exLogFile.appendLine(fileOne.getName());
+		  
+		  exLogFile.appendLine(fileTwo.getName());
+		  
+		  exLogFile.appendLine(fileThree.getName());
+		  
+		  
+		  
+		  //assert that we only have three Meta data lines in the log file
+		  assertTrue(exLogFile.countMetaDataLines() == 3);
+		  
+		
+	}
+	
+	//Ensure meta data lines are correctly measured, even when file names are written before meta data
+	@Test
+	public void test_reverse_countMetaDataLines() throws IOException {
+		
+		File fileOne = input_folder.newFile("1.czi");
+		  
+		  File fileTwo = input_folder.newFile("2.czi");
+		  
+		  File fileThree = input_folder.newFile("3.czi");
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+		  
+		//write file names (pretending we analyzed these files)
+		  exLogFile.appendLine(fileOne.getName());
+		  
+		  exLogFile.appendLine(fileTwo.getName());
+		  
+		  exLogFile.appendLine(fileThree.getName());
+		  
+		//write metadata 
+		  exLogFile.writeMetaData("input_directory", exLogFile.getInputDirectory().toString());
+		  
+		  exLogFile.writeMetaData("output_directory", exLogFile.whichFile().getParent());
+		  
+		  exLogFile.writeMetaData("extension", exLogFile.getExtension());
+		  
+		  
+		  
+		  //assert that we only have three Meta data lines in the log file
+		  assertTrue(exLogFile.countMetaDataLines() == 3);
+		  
+		
+	}
+	
+	
+	//Ensure that 0 is returned when no meta data lines have been written
+	@Test
+	public void test_zero_countMetaDataLines() throws IOException {
+		
+		File fileOne = input_folder.newFile("1.czi");
+		  
+		  File fileTwo = input_folder.newFile("2.czi");
+		  
+		  File fileThree = input_folder.newFile("3.czi");
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+		  
+		//write file names (pretending we analyzed these files)
+		  exLogFile.appendLine(fileOne.getName());
+		  
+		  exLogFile.appendLine(fileTwo.getName());
+		  
+		  exLogFile.appendLine(fileThree.getName());
+		  
+		  
+		  
+		  
+		  //assert that we have zero Meta data lines in the log file
+		  assertTrue(exLogFile.countMetaDataLines() == 0);
+		  
+		
+	}
 	
 	
 	
+	//Ensure that 0 is returned when the file is empty
+	@Test
+	public void test_empty_countMetaDataLines() throws IOException {
+		
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+
+		  
+		  //assert that we have zero Meta data lines in the log file
+		  assertTrue(exLogFile.countMetaDataLines() == 0);	  
+		
+	}
 	
-	 }
+	
+	//Ensure that the analyzed files count is correct
+		@Test
+		public void test_countAnalyzedFiles() throws IOException {
+			
+			File fileOne = input_folder.newFile("1.czi");
+			  
+			  File fileTwo = input_folder.newFile("2.czi");
+			  
+			  File fileThree = input_folder.newFile("3.czi");
+			  
+			  File log = output_folder.newFile("myLogFile.txt");
+			  
+			  String extension = "czi";
+			  
+			  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+			  
+			//write metadata 
+			  exLogFile.writeMetaData("input_directory", exLogFile.getInputDirectory().toString());
+			  
+			  exLogFile.writeMetaData("output_directory", exLogFile.whichFile().getParent());
+			  
+			  exLogFile.writeMetaData("extension", exLogFile.getExtension());
+			  
+			//write file names (pretending we analyzed these files)
+			  exLogFile.appendLine(fileOne.getName());
+			  
+			  exLogFile.appendLine(fileTwo.getName());
+			  
+			  exLogFile.appendLine(fileThree.getName());
+			  
+			  
+			
+			  //assert that we correctly counted the three analyzed file name lines (i.e. the three analyzed files)
+			  assertTrue(exLogFile.countAnalyzedFiles() == 3);
+			  
+			
+		}
+		
+		
+//Ensure that the analyzed files count is correct when meta data lines and file names are written in reverse order
+	@Test
+	public void test_reverse_countAnalyzedFiles() throws IOException {
+		
+		File fileOne = input_folder.newFile("1.czi");
+		  
+		  File fileTwo = input_folder.newFile("2.czi");
+		  
+		  File fileThree = input_folder.newFile("3.czi");
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+		//write file names (pretending we analyzed these files)
+		  exLogFile.appendLine(fileOne.getName());
+		  
+		  exLogFile.appendLine(fileTwo.getName());
+		  
+		  exLogFile.appendLine(fileThree.getName());
+		  
+		//write metadata 
+		  exLogFile.writeMetaData("input_directory", exLogFile.getInputDirectory().toString());
+		  
+		  exLogFile.writeMetaData("output_directory", exLogFile.whichFile().getParent());
+		  
+		  exLogFile.writeMetaData("extension", exLogFile.getExtension());
+		  
+		  
+		  //assert that we correctly counted the three analyzed file name lines (i.e. the three analyzed files)
+		  assertTrue(exLogFile.countAnalyzedFiles() == 3);
+		  
+		
+	}
+
+	
+	
+	//Ensure that 0 is returned when the file is empty
+	@Test
+	public void test_empty_countAnalyzedFiles() throws IOException {
+		
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  
+
+		//assert that we have zero analyzed file name lines in the log file
+		  assertTrue(exLogFile.countAnalyzedFiles() == 0);	  
+	}
+		
+		  
+	//Ensure that 0 is returned when no file names have been written
+	@Test
+	public void test_zero_countAnalyzedFiles() throws IOException {
+		
+		File fileOne = input_folder.newFile("1.czi");
+		  
+		  File fileTwo = input_folder.newFile("2.czi");
+		  
+		  File fileThree = input_folder.newFile("3.czi");
+		  
+		  File log = output_folder.newFile("myLogFile.txt");
+		  
+		  String extension = "czi";
+		  
+		  LogFile exLogFile = new LogFile(log, input_folder.getRoot(), extension);
+		  		  
+		  
+		//write metadata 
+		  exLogFile.writeMetaData("input_directory", exLogFile.getInputDirectory().toString());
+		  
+		  exLogFile.writeMetaData("output_directory", exLogFile.whichFile().getParent());
+		  
+		  exLogFile.writeMetaData("extension", exLogFile.getExtension());
+
+		  
+		  //assert that we have zero analyzed file name lines in the log file
+		  assertTrue(exLogFile.countAnalyzedFiles() == 0);
+		  
+		
+	}
+	
+	
+}
+
+	
+
+	
+	
+	
+	 
 	
 
 
